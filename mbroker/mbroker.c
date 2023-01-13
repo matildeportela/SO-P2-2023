025c3636f28df45cd6d080e0df2440811e973280
Matilde Portela <matildeportela@icloud.com>
2023-01-13 15:28:44 +0000

    * mbroker switchcase read()

--- mbroker/mbroker.c
+++ mbroker/mbroker.c
@@ -17,6 +17,7 @@
 
 int main(int argc, char **argv) {
     if(argc != 3){
+        fprintf(stderr, "usage: mbroker <pipename>\n");
         return -1;
     }
     
@@ -24,7 +25,7 @@ int main(int argc, char **argv) {
     char register_pipe_name[256];
 
     strncpy(register_pipe_name, argv[1], 256);
-    int max_sessions = atoi(argv[2]);
+    //int max_sessions = atoi(argv[2]);
 
     if (unlink(register_pipe_name) != 0 && errno != ENOENT) {
         fprintf(stderr, "[ERR]: unlink(%s) failed: %s\n", register_pipe_name,
@@ -43,21 +44,87 @@ int main(int argc, char **argv) {
         fprintf(stderr, "[ERR]: open failed: %s\n", strerror(errno));
         return -1;
     }
+    
     char register_request[289];
-    read(rx , register_request, 289);
+    if (read(rx , register_request, 289) == -1){
+        return -1;
+    }
+    
     close(rx);
+    
+    int opcode;
+    char named_pipe[256];
+    char box_name[32];
+    int return_code;
+    char error_message[1034];
+    char c;
 
+
+
+    read(register_request, opcode, 1);
     
+    switch (opcode)
+    {
+    case(1):
+    //pedido de registo de publisher
+        read(register_request ,named_pipe, 256);
+        read(register_request, box_name, 32);
+
+    break;
+
+    case(2):
+    //pedido de registo de subscriber
+        read(register_request,named_pipe, 256);
+        read(register_request, box_name, 32);
     
+    break;
+
+    case(3):
+    //pedido de criação de caixa
+        read(register_request,named_pipe, 256);
+        read(register_request, box_name, 32);
+        
+    break;
+
+    case(4):
+    //resposta ao pedido de criação de caixa
+        //return_code é 0 (sucesso) ou 1 (ERRO)
+        read(register_request, c, 1);
+        return_code = atoi(c);
+        read(register_request, error_message, 1024);
+        
     
+    break;
+
+    case(5):
+    //pedido de remoção de caixa
+        read(register_request, named_pipe, 256);
+        read(register_request, box_name, 32);
     
+    break;
+
+    case(6):
+    //resposta ao pedido de remoção de caixa
+        //return_code é 0 (sucesso) ou 1 (ERRO)
+        read(register_request, return_code, 1);
+        read(register_request, error_message, 1024);
+
+    break;
+
+    case(7):
+    //pedido de listagem de caixas
+        read(register_request, named_pipe, 256);
+
+    break;
+
+    case(8):
+    //resposta à listagem de caixas vem em várias mensagens
+    //DUVIDA: meio confuso perguntar ao prof?????
+    break;
+    }
     
-    
-    
-    
-    
-    
-    fprintf(stderr, "usage: mbroker <pipename>\n");
-    WARN("unimplemented"); // TODO: implement
-    return -1;
+
+
+
+    return 0;
 }
