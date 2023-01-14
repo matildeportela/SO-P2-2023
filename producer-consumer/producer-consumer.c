02eea30c41a8e5a19dbd6d3593ab35d56b187410
Matilde Portela <matildeportela@icloud.com>
2023-01-14 23:27:12 +0000

    * enqueue e dequeue implemented :)

--- producer-consumer/producer-consumer.c
+++ producer-consumer/producer-consumer.c
@@ -9,11 +9,10 @@ pthread_cond_t podeProd;
 pthread_cond_t podeCons;
 
 
+int count = 0;
 
-int buf[queue->pcq_capacity];
-int not_full=0;
-int consptr=0;
-int count=0;
+pthread_cond_t not_empty;
+pthread_cond_t not_full;
 
 
 // pcq_enqueue: insert a new element at the front of the queue
@@ -21,29 +20,43 @@ int count=0;
 // If the queue is full, sleep until the queue has space
 
 int pcq_enqueue(pc_queue_t *queue, void *elem) {
-    while(1) {
-        int item = produz();
-        pthread_mutex_lock(&queue->pcq_current_size_lock);
+    void *buf[queue->pcq_capacity];
     
-        while (&queue->count == &queue->pcq_capacity) {
-            pthread_cond_wait(&queue->not_full, queue->pcq_current_size_lock);
-        }
+    pthread_mutex_lock(&queue->pcq_current_size_lock);
 
-        queue->pcq_buffer[queue->pcq_head] = elem;
-        queue->pcq_head = (queue->pcq_head + 1) % queue->pcq_capacity;
-        queue->count++;
-
-        pthread_cond_signal(&queue->not_empty);
-        pthread_mutex_unlock(queue->pcq_current_size_lock);
+    while (count == queue->pcq_capacity) {
+        pthread_cond_wait(&not_full, &mutex);
     }
+
+    buf[queue->pcq_head] = elem;
+    queue->pcq_head = (queue->pcq_head + 1) % queue->pcq_capacity;
+    count++;
+
+    pthread_cond_signal(&not_empty);
+    pthread_mutex_unlock(&mutex);
+
     return 0;
-}
+    }
+  
+
 // pcq_dequeue: remove an element from the back of the queue
 //
 // If the queue is empty, sleep until the queue has an element
-void *pcq_dequeue(pc_queue_t *queue){
-   int a;
-   int b;
-   b = a + 1;
+
+void *pcq_dequeue(pc_queue_t *queue) {
+    void *elem = NULL;
+    pthread_mutex_lock(&queue->pcq_current_size_lock);
+    while (count == 0) {
+        pthread_cond_wait(&not_empty, &queue->pcq_current_size_lock);
+    }
+    
+    elem = queue->pcq_buffer[queue->pcq_tail];
+    queue->pcq_tail = (queue->pcq_tail + 1) % queue->pcq_capacity;
+    count--;
+    
+    pthread_cond_signal(&not_full);
+    pthread_mutex_unlock(&queue->pcq_current_size_lock);
+    
+    return elem;
 }
 
